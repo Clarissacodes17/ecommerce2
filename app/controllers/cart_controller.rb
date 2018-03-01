6f8598481476c86718ead3afca8dec6c1b66e6fe
Clarissa Thomas <clarissa.thomas2009@gmail.com>
2018-03-01 09:20:38 -0500

    added sessions for users

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,14 +3,21 @@ class CartController < ApplicationController
 
   def add_to_cart
 
-  	line_items = LineItem.all
-  	 	if line_items.any? {|l| l.product_id == params[:product_id].to_i }
-	 			line_item = LineItem.find_by(product_id: params[:product_id])
+    @order = current_order
+
+  	line_item = @order.line_items.find_by(product_id: params[:product_id])
+
+  	 	if !line_item.blank?
+	 			# line_item = LineItem.find_by(product_id: params[:product_id])
 	 			line_item.update(quantity: line_item.quantity += params[:quantity].to_i)
 	 			line_item.update(line_item_total: line_item.quantity * line_item.product.price)
 
   	 	else	
-		  	line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
+		  	line_item = @order.line_items.new(product_id: params[:product_id], quantity: params[:quantity])
+
+        @order.save
+        session[:order_id] = @order.id
+
 		  	line_item.update(line_item_total: line_item.quantity * line_item.product.price)
 		  end
   		redirect_back(fallback_location: root_path)
@@ -18,29 +25,40 @@ class CartController < ApplicationController
   end
 
   def view_order
-  	@line_items = LineItem.all
+  	@line_items = current_order.line_items
   end
 
   def checkout
-  	line_items = LineItem.all
-  	@order = Order.create(user_id: current_user.id, subtotal: 0)
+  	line_items = current_order.line_items
 
-  	line_items.each do |line_item|
-  		line_item.product.update(quantity: ( line_item.product.quantity - line_item.quantity))
-  		@order.order_items[line_item.product_id] = line_item.quantity
-  		@order.subtotal += line_item.line_item_total
+    unless line_items.length == 0
+
+  	   current_order.update(user_id: current_user.id, subtotal: 0)
+
+        @order = current_order
+
+  	   line_items.each do |line_item|
+  		    line_item.product.update(quantity: ( line_item.product.quantity - line_item.quantity))
+  		    @order.order_items[line_item.product_id] = line_item.quantity
+  		    @order.subtotal += line_item.line_item_total
   	end
   	
-  	@order.save
-  	@order.update(sales_tax: @order.subtotal * 0.06)
-  	@order.update(grand_total: @order.subtotal + @order.sales_tax)	
+  	     @order.save
+  	     @order.update(sales_tax: @order.subtotal * 0.06)
+  	     @order.update(grand_total: @order.subtotal + @order.sales_tax)	
+         # session[:order_id] = nil
 
-  	line_items.destroy_all
+  	# line_items.destroy_all
   end
+end
 
   def order_complete
-     @order = Order.find(params[:order_id])
+    @order = Order.find(params[:order_id])
     @amount = (@order.grand_total.to_f.round(2) * 100).to_i
+    
+    line_items.destroy_all
+    session[:order_id] = nil
+
 
     customer = Stripe::Customer.create(
       :email => current_user.email,
